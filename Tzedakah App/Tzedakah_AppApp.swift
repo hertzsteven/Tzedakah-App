db3e36f5c20543b145f160b37f04726633e62034
Steven Hertz <StevenH@appi4.com>
2024-06-05 15:39:55 -0400

    Initial Commit

---
+++ Tzedakah App/Tzedakah_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tzedakah_AppApp.swift
+//  Tzedakah App
+//
+//  Created by Steven Hertz on 6/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Tzedakah_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
