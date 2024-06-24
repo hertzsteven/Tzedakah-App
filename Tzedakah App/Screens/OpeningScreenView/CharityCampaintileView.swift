b62285c52f4505c0bd231b8872e2db9329885ab9
Steven Hertz <StevenH@appi4.com>
2024-06-24 19:00:10 -0400

    after eliminating navidation error

--- Tzedakah App/Screens/OpeningScreenView/CharityCampaintileView.swift
+++ Tzedakah App/Screens/OpeningScreenView/CharityCampaintileView.swift
@@ -110,78 +110,23 @@ struct CharityCampaintileView: View {
                         Spacer()
                         
                          DonateNowButton3(itm: item)
-//                        .navigationDestination(for: CharityCampaign.self) { campaign in
-//                            CampaignDetailScreenView(charityCampaign: campaign)
-//        //                  Text("This is the bbbb next view. and color is 1 \(campaign.name)")
-//                        }
 
-                        .navigationDestination(for: Campaign.self) { campaign in
-                            CampaignDetailScreenView(campaign: campaign)
-        //                  Text("This is the bbbb next view. and color is 1 \(campaign.name)")
-                        }
-                        
                         
                     }
                 }
           }
-        
- /*
-  ForEach(charityCampaigns) { item in
-          RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/).fill(Color.white) // Use fill instead of stroke
-            .frame(width: 300, height: 400)
-            .shadow(color: Color.black.opacity(0.3), radius: 10, x: 0, y: 15) // Adding shadow here
-            .overlay {
-              VStack {
-                Image(item.imageName)
-                  .resizable()
-                  .aspectRatio(contentMode: .fill)
-                  .frame(width: 300, height: 250)
-                  .clipShape(UnevenRoundedRectangle(cornerRadii: .init(topLeading: 25, topTrailing: 25)))
-                  .overlay(alignment: .bottom) {
-                    Text(item.name)
-                      .font(.system(size: 28, weight: .semibold))
-                      .foregroundStyle(.white)
-                      .padding()
-                      .frame(maxWidth: .infinity, alignment: .bottomLeading)
-                  }
-                
-                HStack {
-                  Text("conflict")
-                    .padding(.horizontal,8)
-                  
-                    .background(Color(.systemGray6))
-                    .clipShape(Capsule())
-                    .padding(.leading,12)
-                  
-                  Text("Emergency")
-                    .padding(.horizontal,8)
-                    .background(Color(.systemGray6))
-                    .clipShape(Capsule())
-                  
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .foregroundStyle(.gray)
-                Spacer()
-                
-                  DonateNowButton2(itm: item)
-                .navigationDestination(for: CharityCampaign.self) { campaign in
-                    CampaignDetailScreenView(charityCampaign: campaign)
-//                  Text("This is the bbbb next view. and color is 1 \(campaign.name)")
-                }
-                
-              }
-
-            }
-            
-        }
-  */
       }
       .scrollTargetLayout()
       .frame( height: 430, alignment: .top)
     }
     .scrollTargetBehavior(.viewAligned)
     .safeAreaPadding(.horizontal, 20.0)
+    .navigationDestination(for: Campaign.self) { campaign in
+        CampaignDetailScreenView(campaign: campaign)
+    }
+
   }
+    
 }
 
 #Preview {
